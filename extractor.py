d2e41fb1c3eccfa0aee96759e82f79adc1e5247c
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-02-02 15:37:10 -0300

    Add some PEP8 compliance

--- extractor.py
+++ extractor.py
@@ -9,17 +9,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             description='''A script that extracts contents from a csv file
             into an sqlite database''')
-    parser.add_argument('csv_file', type=argparse.FileType('r'), help='Csv file to be imported')
+    parser.add_argument('csv_file',
+                        type=argparse.FileType('r'),
+                        help='Csv file to be imported')
     args = parser.parse_args()
 
     with args.csv_file as csv_file:
         # When used as a context manager, a sqlite connection already commits
         # the transaction, yet it does not close it. That's why it is used
         # combined with contextlib.closing
-        with contextlib.closing(sqlite3.connect('{}.db'.format(os.path.basename(csv_file.name)))) as conn:
+        db_name = '{}.db'.format(os.path.basename(csv_file.name))
+        with contextlib.closing(sqlite3.connect(db_name)) as conn:
             with conn as connection:
                 cursor = connection.cursor()
-                rows = ([value.strip() for value in line.split(',')] for line in csv_file)
+                rows = ([value.strip() for value in line.split(',')]
+                        for line in csv_file)
                 cursor.execute(""" CREATE TABLE IF NOT EXISTS person (
                                                 time timestamp PRIMARY KEY,
                                                 name varchar NOT NULL,
